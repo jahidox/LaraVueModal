55bb42c780b5589c70d10c12011b8b5627786684
jhilario <desarrollador3@busmen.com>
2019-03-20 16:34:57 -0600

    Seguridad en apis

--- app/Http/Controllers/API/UserController.php
+++ app/Http/Controllers/API/UserController.php
@@ -9,6 +9,9 @@ use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller
 {
+    public function __construct(){
+        $this->middleware('auth:api');
+    }
 
     public function index() {
         return User::latest()->paginate(5);
@@ -33,15 +36,30 @@ class UserController extends Controller
         ]);
     }
 
+    public function profile()
+    {
+        return auth('api')->user();
+    }
+    
     public function show($id){
         //
     }
 
     public function update(Request $request, $id){
-        //
+        $user = User::findOrFail($id);
+        $this->validate($request, [
+            'name' => 'required|string|max:120',
+            'bio' => 'required|string|max:120',
+            'password' => 'required|string|min:6|max:10', 
+            'email' => 'required|string|email|max:120|unique:users,email,'.$user->id,
+    	]);
+        $user->update($request->all());
+        return ['message'=> 'Updated the user info'];
     }
 
     public function destroy($id){
-        //Prueba desde otra cuenta de github
+        $user = User::findOrFail($id);
+        $user->delete();
+        return ['message'=> 'User deleted'];
     }
 }
--- resources/js/app.js
+++ resources/js/app.js
@@ -28,12 +28,16 @@ const toast = swal.mixin({
 window.toast = toast;
 
 
+window.Fire = new Vue();
+
+
 Vue.use(VueRouter)
 
 let routes = [
     { path: '/dashboard', component: require('./components/Dashboard.vue').default },
     { path: '/profile', component: require('./components/Profile.vue').default },
-    { path: '/users', component: require('./components/Users.vue').default }
+    { path: '/users', component: require('./components/Users.vue').default },
+    { path: '/developer', component: require('./components/Developer.vue').default }
 ]
 
 const router = new VueRouter({
@@ -51,6 +55,21 @@ Vue.filter('myDate', function (created) {
 
 // const files = require.context('./', true, /\.vue$/i)
 // files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default))
+
+Vue.component(
+  'passport-clients',
+  require('./components/passport/Clients.vue').default
+);
+
+Vue.component(
+  'passport-authorized-clients',
+  require('./components/passport/AuthorizedClients.vue').default
+);
+
+Vue.component(
+  'passport-personal-access-tokens',
+  require('./components/passport/PersonalAccessTokens.vue').default
+);
 
 Vue.component('example-component', require('./components/ExampleComponent.vue').default);
 
